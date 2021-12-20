97929198f63a2c65177161f1411134ac47a7a29c
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-12-20 16:32:11 +0100

    Added comments and cleared up structure even more

--- app.py
+++ app.py
@@ -8,19 +8,10 @@ from definitions import DISTRIBUTION, RESOURCES, CONFIG_FILE_NAME
 from src.handlers.Dispatcher import Dispatcher
 from src.modules.PrototypeConnection import PrototypeConnection
 from src.modules.google_api.GoogleApiWrapper import GoogleApiWrapper
-from src.helpers.Logger import Logger
 from src.routes.Routes import init_views
-
-
-if os.environ.keys().__contains__('FLASK_ENV') and os.environ['FLASK_ENV'] == "development" and DISTRIBUTION:
-    Logger.log_error("Running in development mode with DISTRIBUTION set to true!")
-    raise RuntimeError("Quiting execution!")
-elif os.environ.keys().__contains__('FLASK_ENV') and os.environ['FLASK_ENV'] == "production" and not DISTRIBUTION:
-    Logger.log_error("Running in production mode with DISTRIBUTION set to false!")
-    raise RuntimeError("Quiting execution!")
-
-
 import logging
+
+
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -42,26 +33,31 @@ def initialize(application):
     # Check if google api is working correctly
     GoogleApiWrapper(credentials_path=os.path.join(RESOURCES, 'gcloud_credentials.json'))
 
+    # Initialize routes
     init_views(application, dispatcher)
 
+    # If running in distribution mode, then set standard routes and launch browser
+    if DISTRIBUTION:
+        @app.route('/', methods=['GET'])
+        def standard_route():
+            return render_template("index.html")
 
+        @app.errorhandler(404)
+        @app.errorhandler(500)
+        def error_route(e):
+            return render_template("index.html")
+
+        log.setLevel(logging.INFO)
+        import webbrowser
+        webbrowser.open("http://localhost:5000")
+
+
+# Initialize flask app
 app = Flask(__name__)
+
+# Initialize CORS
 CORS(app)
+
+# Initialize rest of the backend
 initialize(app)
-app.run(debug=False, threaded=True)
 
-
-if DISTRIBUTION:
-    @app.route('/', methods=['GET'])
-    def standard_route():
-        return render_template("index.html")
-
-
-    @app.errorhandler(404)
-    @app.errorhandler(500)
-    def error_route(e):
-        return render_template("index.html")
-
-    log.setLevel(logging.INFO)
-    import webbrowser
-    webbrowser.open("http://localhost:5000")
